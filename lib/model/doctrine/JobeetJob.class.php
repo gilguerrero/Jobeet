29c7c222f88d2bf307cc6e39c5e25de269016a4b
Gilberto Guerrero <guerrero7@outlook.es>
2015-05-06 08:55:40 -0500

    Formularios (pruebas y cambios) day 11
    Cambios en algunos archivos para mejorar su funcionamiento y correción
    de errores.

--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -28,6 +28,20 @@ class JobeetJob extends BaseJobeetJob
 		return parent::save($conn);
 	}
 
+	public function extend()
+	{
+		if (!$this->expiresSoon())
+		{
+		  return false;
+		}
+
+		$this->setExpiresAt(date('Y-m-d', time() + 86400 * sfConfig::get('app_active_days')));
+
+		$this->save();
+
+		return true;
+	}
+
 	public function __toString()
 	{
 		return sprintf('%s at %s (%s)', $this->getPosition(), $this->getCompany(), $this->getLocation());
--- test/functional/frontend/jobActionsTest.php
+++ test/functional/frontend/jobActionsTest.php
@@ -17,3 +17,79 @@ $browser->
     checkElement('body', '!/This is a temporary page/')->
   end()
 ;
+
+$browser->info('3 - Post a Job page')->
+  info('  3.1 - Submit a Job')->
+ 
+  get('/job/new')->
+  with('request')->begin()->
+    isParameter('module', 'job')->
+    isParameter('action', 'new')->
+  end()
+;
+
+$browser->info('3 - Post a Job page')->
+  info('  3.1 - Submit a Job')->
+ 
+  get('/job/new')->
+  with('request')->begin()->
+    isParameter('module', 'job')->
+    isParameter('action', 'new')->
+  end()->
+ 
+  click('Preview your job', array('job' => array(
+    'company'      => 'Sensio Labs',
+    'url'          => 'http://www.sensio.com/',
+    'logo'         => sfConfig::get('sf_upload_dir').'/jobs/sensio-labs.gif',
+    'position'     => 'Developer',
+    'location'     => 'Atlanta, USA',
+    'description'  => 'You will work with symfony to develop websites for our customers.',
+    'how_to_apply' => 'Send me an email',
+    'email'        => 'for.a.job@example.com',
+    'is_public'    => false,
+  )))->
+ 
+  with('request')->begin()->
+    isParameter('module', 'job')->
+    isParameter('action', 'create')->
+  end()
+;
+
+$browser->info('  3.6 - A job validity cannot be extended before the job expires soon')->
+  createJob(array('position' => 'FOO4'), true)->
+  call(sprintf('/job/%s/extend', $browser->getJobByPosition('FOO4')->getToken()), 'put', array('_with_csrf' => true))->
+  with('response')->begin()->
+    isStatusCode(404)->
+  end()
+;
+ 
+$browser->info('  3.7 - A job validity can be extended when the job expires soon')->
+  createJob(array('position' => 'FOO5'), true)
+;
+ 
+$job = $browser->getJobByPosition('FOO5');
+$job->setExpiresAt(date('Y-m-d'));
+$job->save();
+ 
+$browser->
+  call(sprintf('/job/%s/extend', $job->getToken()), 'put', array('_with_csrf' => true))->
+  with('response')->isRedirected()
+;
+ 
+$job->refresh();
+$browser->test()->is(
+  $job->getDateTimeObject('expires_at')->format('y/m/d'),
+  date('y/m/d', time() + 86400 * sfConfig::get('app_active_days'))
+);
+
+$browser->
+  get('/job/new')->
+  click('Preview your job', array('job' => array(
+    'token' => 'fake_token',
+  )))->
+ 
+  with('form')->begin()->
+    hasErrors(7)->
+    hasGlobalError('extra_fields')->
+  end()
+;
