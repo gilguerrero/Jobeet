21a9ca287b0ee5a1e2659c0a8dc215028b82f6f9
Gilberto Guerrero <guerrero7@outlook.es>
2015-05-06 17:09:12 -0500

    Servicio Web day 15
    Servicio web funcionando.

--- lib/model/doctrine/JobeetAffiliateTable.class.php
+++ lib/model/doctrine/JobeetAffiliateTable.class.php
@@ -7,11 +7,14 @@
  */
 class JobeetAffiliateTable extends Doctrine_Table
 {
-    /**
-     * Returns an instance of this class.
-     *
-     * @return object JobeetAffiliateTable
-     */
+    public function countToBeActivated()
+	{
+		$q = $this->createQuery('a')
+		  ->where('a.is_active = ?', 0);
+
+		return $q->count();
+	}
+	
     public static function getInstance()
     {
         return Doctrine_Core::getTable('JobeetAffiliate');
