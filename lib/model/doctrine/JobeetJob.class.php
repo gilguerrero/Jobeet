cdb26edf7b2c97cfaad25680538f85ade30ac578
Gilberto Guerrero <guerrero7@outlook.es>
2015-05-06 10:44:30 -0500

    Generador admin day 12
    Desarrollo de una completa interfaz para el backend de Jobeet.

--- lib/model/doctrine/JobeetJob.class.php
+++ lib/model/doctrine/JobeetJob.class.php
@@ -30,16 +30,15 @@ class JobeetJob extends BaseJobeetJob
 
 	public function extend()
 	{
-		if (!$this->expiresSoon())
-		{
-		  return false;
-		}
-
-		$this->setExpiresAt(date('Y-m-d', time() + 86400 * sfConfig::get('app_active_days')));
-
-		$this->save();
-
-		return true;
+		if (!$force && !$this->expiresSoon())
+	    {
+	      return false;
+	    }
+	 
+	    $this->setExpiresAt(date('Y-m-d', time() + 86400 * sfConfig::get('app_active_days')));
+	    $this->save();
+	 
+	    return true;
 	}
 
 	public function __toString()
